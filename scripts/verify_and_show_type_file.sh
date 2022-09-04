0be1aff8794a280d1a15eac901225719f74329eb
Fernanda Lisboa <fernandanlisboa@gmail.com>
2022-09-04 17:31:51 -0300

    fix: verifying syntax

--- scripts/verify_and_show_type_file.sh
+++ scripts/verify_and_show_type_file.sh
@@ -4,15 +4,15 @@ ARG=$1
 return_=$(scripts/verify_file_type.sh "$ARG")
 type=""
 
-if [ $return_==0 ]; then
+if [[ "$return_" == 0 ]]; then
     type="Arquivo padrão"
 
 
-elif [ $return_==1 ]; then
+elif [[ "$return_" == 1 ]]; then
     type="Diretório"
 
 
-elif [ $return_==2 ]; then
+elif [[ "$return_" == 2 ]]; then
     type="Executável"
 else
     type="Outros"
