d4bbc77e1df417b72f7ad96c275794d77cfbcc64
Fernanda Lisboa <fernandanlisboa@gmail.com>
2022-09-04 17:20:02 -0300

    fix: executation call to another script

---
+++ scripts/verify_and_show_type_file.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+ARG=$1
+
+return_=$(scripts/verify_file_type.sh "$ARG")
+type=""
+
+if [ $return_==0 ]; then
+    type="Arquivo padrão"
+
+
+elif [ $return_==1 ]; then
+    type="Diretório"
+
+
+elif [ $return_==2 ]; then
+    type="Executável"
+else
+    type="Outros"
+fi
+
+echo $type
