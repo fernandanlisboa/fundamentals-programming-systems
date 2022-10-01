482dd313bccbedfac8ebca4432ce0a48f3da0987
Fernanda Lisboa <fernandanlisboa@gmail.com>
2022-10-01 20:34:22 -0300

    fix: debug_log method

--- scripts/debug_log.sh
+++ scripts/debug_log.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
+set -e
 tmp_dir='logs'
 if [ ! -d "$tmp_dir" ]; then
     mkdir $tmp_dir
 fi
 
-exec > "$tmp_dir/output.txt" 2>&1
 FILE=scripts/create_directory.sh
 SCRIPT_NAME=scripts/verify_and_show_type_file.sh
+SAVE_="$tmp_dir/log"
 
-# exec 1> >(logger -s -t $("$SCRIPT_NAME" "$FILE")) 2>&1
-RESULT=$($SCRIPT_NAME $FILE | tee >(cat >&2))
 
-if $result; then
-    echo "$RESULT"
-else
-    exit
-fi
+bash -x -e $("$SCRIPT_NAME" "$FILE") 2>"$SAVE_"
+logger -f "$SAVE_"
 
