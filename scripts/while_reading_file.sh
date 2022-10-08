7f832e0638ddff4e71fdd929c44fa1a4a8554d8d
Fernanda Lisboa <fernandanlisboa@gmail.com>
2022-10-08 17:24:38 -0300

    add comments in while_reading_file.sh

--- scripts/while_reading_file.sh
+++ scripts/while_reading_file.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 filename=$1
-n=1
+n=1 # counter
 echo "Reading $filename..."
 while read -u 3 line;
 do
 # reading each line
 echo "$line"
 if [ $(($n % 10)) = 0 ]; then
+# user interaction
 read -p "Do you want proceed? [y/n]"$'\n' answer
     if [ ! $answer == 'y' ]; then
         break
@@ -14,5 +15,4 @@ read -p "Do you want proceed? [y/n]"$'\n' answer
 fi
 n=$((n+1))
 done 3< $filename
-
 exit 0
