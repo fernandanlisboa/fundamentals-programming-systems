d44d4ae191998066da1e311b8015898731224037
Fernanda Lisboa <fernandanlisboa@gmail.com>
2022-11-08 18:40:51 -0300

    up

--- scripts/municipios_ba.sh
+++ scripts/municipios_ba.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-filename=covid.csv
-dir=/municipios
+filename=covid_.csv
+dir=./municipios
+echo $dir
 if [ ! -d "$dir" ]; then
+  echo "$dir"
   mkdir "$dir"
 fi
 awk -F ";" '{
   if($2 == "BA" && $3 != ""){
     file="$dir/$3.csv"
-    print $0 > $file
+    print $0 > $files
     }
 }' $filename
