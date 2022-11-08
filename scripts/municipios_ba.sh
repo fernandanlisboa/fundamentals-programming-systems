a0e5176831ac2d3947548076eaa0204acfbaaeca
Fernanda Lisboa <fernandanlisboa@gmail.com>
2022-11-08 20:30:54 -0300

    fix: towns creation

--- scripts/municipios_ba.sh
+++ scripts/municipios_ba.sh
@@ -4,10 +4,11 @@ dir=municipios
 if [ ! -d "./$dir" ]; then
   mkdir "$dir"
 fi
-awk -F ";" '{
+awk -F ";" -v dir=$dir '{
   if($2 == "BA" && $3 != ""){
-    file="./${dir}/${3}.csv"
-    print $file
-    # print $1 >> $file
+    # print "files: " dir;
+    file=dir "/" $3 ".csv";
+    # print $3 "-" file;
+    print $0 >> file;
     }
 }' $filename
