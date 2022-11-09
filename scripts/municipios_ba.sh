fdbfa3f395ab15fe15b360c2d2dfc9184ae46a66
Fernanda Lisboa <fernandanlisboa@gmail.com>
2022-11-08 21:16:18 -0300

    done municipios aula 0711

--- scripts/municipios_ba.sh
+++ scripts/municipios_ba.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-filename=covid_.csv
+filename=covid.csv
 dir=municipios
 if [ ! -d "./$dir" ]; then
   mkdir "$dir"
@@ -12,3 +12,26 @@ awk -F ";" -v dir=$dir '{
     print $0 >> file;
     }
 }' $filename
+
+SAVE_=$IFS
+IFS=$(echo -en "\n\b")
+
+files="$dir/*"
+# 2ª parte
+for f in $files
+  do
+  file=${f##*/}
+    awk -F ";" -v cidade=${file%.*} 'BEGIN{
+      maior=0; 
+      data="";
+      }
+      {
+        if($14 > maior){
+          maior=$14;
+          data=$8;
+        }
+      }
+      END{
+        printf "%s;%s;%d;\n",cidade,data,maior; 
+      }' $f >> final.csv
+  done
